f159f349a6b1d36ef6ec86a5536b0bed60f751c8
alren3 <alrenvinceav@gmail.com>
2024-08-06 20:47:55 +0800

    Create try.php

---
+++ try.php
@@ -0,0 +1,20 @@
+<?php
+function getClientIp() {
+    // Check for shared internet/ISP IP
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }
+    // Check for IPs passing through proxies
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    // Get the remote address
+    else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$client_ip = getClientIp();
+echo "Client IP Address: " . $client_ip;
+?>
